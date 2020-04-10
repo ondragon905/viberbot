60da3749e7db1fbd185c53d455ac5e77d8f57900
ondragon905 <60470043+ondragon905@users.noreply.github.com>
2020-04-10 19:14:11 +0300

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -3,7 +3,7 @@ from viberbot import Api
 from viberbot.api.bot_configuration import BotConfiguration
 
 bot_configuration = BotConfiguration(
-                    name='FeaR',
+                    name='TranslatorBot',
                     avatar='https://viber.com/avatar/jpg',
                     auth_token=TOKEN
 )
