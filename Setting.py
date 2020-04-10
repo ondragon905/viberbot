e325d97ab3dba9a38dd22266a1cfad56c2def05e
ondragon905 <60470043+ondragon905@users.noreply.github.com>
2020-04-10 19:11:23 +0300

    Update Setting.py

--- Setting.py
+++ Setting.py
@@ -1,3 +1,3 @@
-TOKEN = "4ac78dc1d427d6cb-7b05fa114f58ee0c-f2dc79b919958974"
-URL = "https://viberbottest123.herokuapp.com/"
+TOKEN = "4b5935e1ee67d26f-20c0b4dd5e7ad1ba-f7b8a431aa26ebb5"
+URL = "https://translatorbot112.herokuapp.com/"
 WEBHOOK = URL + '/incoming'
